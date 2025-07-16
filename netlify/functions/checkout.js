478dc1519a6deab61e02c544aecf40721d111992
Dashwarns <dashwarnstore@gmail.com>
2025-07-16 12:52:32 +0800

    Update checkout.js

--- netlify/functions/checkout.js
+++ netlify/functions/checkout.js
@@ -1,1 +1,29 @@
+const fetch = require("node-fetch");
 
+exports.handler = async (event) => {
+  if (event.httpMethod !== "POST") {
+    return { statusCode: 405, body: "Method Not Allowed" };
+  }
+
+  try {
+    const data = JSON.parse(event.body);
+    const url = "https://script.google.com/macros/s/AKfycbzC-zUWzo-pK8L7Ra9pnyuU1AoPUex-ZX3bK1VyGOewFn9VA1qG-bW1b4iXsjjTE3A/exec";
+
+    const response = await fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data)
+    });
+
+    const text = await response.text();
+    return {
+      statusCode: 200,
+      body: text
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: `Error: ${error.message}`
+    };
+  }
+};
